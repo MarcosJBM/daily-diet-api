37726a6da800c6401441ccef6041fdb9a2e2627b
Marcos Botene <marcosjbotene@gmail.com>
2023-12-11 22:33:06 -0300

    feat: login route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import { fastify } from 'fastify';
-import { usersRoutes } from './routes';
+import { fastifyJwt } from '@fastify/jwt';
+import { authRoutes, usersRoutes } from './routes';
+import { env } from './env';
 
 export const app = fastify();
 
+app.register(fastifyJwt, { secret: env.FASTIFY_JWT_SECRET });
+
 app.register(usersRoutes, { prefix: 'users' });
+app.register(authRoutes, { prefix: 'auth' });
---
+++ src/routes/auth.ts
@@ -0,0 +1,45 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { validateSchema } from '../utils';
+import { knexInstance } from '../database';
+import { compare } from 'bcrypt';
+
+const loginBodySchema = z.object({
+  email: z
+    .string({
+      invalid_type_error: 'Email has an invalid type',
+      required_error: 'Email is required',
+    })
+    .min(1, 'Email is required')
+    .email('Email must be valid'),
+  password: z
+    .string({
+      invalid_type_error: 'Password has an invalid type',
+      required_error: 'Password is required',
+    })
+    .min(6, 'Password must be at least 6 characters'),
+});
+
+export async function authRoutes(app: FastifyInstance) {
+  app.post('/login', async (request, reply) => {
+    const result = validateSchema(request.body, loginBodySchema);
+
+    if (typeof result === 'string')
+      return reply.status(400).send({ error: result });
+
+    const { email, password } = result;
+
+    const user = await knexInstance('users').where({ email }).first();
+
+    if (!user) return reply.status(400).send({ error: 'User not found' });
+
+    const isPasswordCorrect = await compare(password, user.hash);
+
+    if (!isPasswordCorrect)
+      return reply.status(400).send({ error: 'Invalid password' });
+
+    const token = app.jwt.sign({ name: user.name, email }, { expiresIn: '7d' });
+
+    return reply.status(200).send({ token });
+  });
+}
