f6ed96defc95634597c970195b41d0326f3fb52d
Marcos Botene <marcosjbotene@gmail.com>
2024-01-30 20:21:12 -0300

    test: test for route that lists users' meals

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -112,4 +112,42 @@ describe('Meals Routes', () => {
     expect(response.status).toEqual(204);
     expect(deletedMeal).toBeUndefined();
   });
+
+  it("should be able to list users' meals", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await supertest(app.server)
+      .post('/meals/create')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Café da manhã',
+        description: 'Suco de laranja e panquecas',
+        date: '2023-01-01',
+        hour: '07:00',
+        is_on_diet: true,
+      });
+
+    await supertest(app.server)
+      .post('/meals/create')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Almoço',
+        description: 'Comida caseira',
+        date: '2023-01-01',
+        hour: '12:00',
+        is_on_diet: true,
+      });
+
+    const response = await supertest(app.server)
+      .get('/meals')
+      .set('Authorization', `Bearer ${token}`);
+
+    const { meals } = response.body;
+
+    expect(response.status).toEqual(200);
+    expect(meals).toEqual([
+      expect.objectContaining({ name: 'Café da manhã' }),
+      expect.objectContaining({ name: 'Almoço' }),
+    ]);
+  });
 });
