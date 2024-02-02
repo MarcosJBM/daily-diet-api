a4b88d45c15c0cebdde49cb9b8d4694a30125017
Marcos Botene <marcosjbotene@gmail.com>
2024-02-01 22:11:27 -0300

    test: add test to user metrics

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -11,6 +11,8 @@ import {
   it,
 } from 'vitest';
 
+import { createAndAuthenticateUser } from '@/utils';
+
 import { app } from '../src/app';
 
 describe('Users routes', () => {
@@ -38,5 +40,57 @@ describe('Users routes', () => {
     });
 
     expect(response.status).toEqual(201);
+  });
+
+  it('should be able to get user metrics', async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await supertest(app.server)
+      .post('/meals/create')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Café da manhã',
+        description: 'Suco de laranja e panquecas',
+        date: '2023-01-01',
+        hour: '07:00',
+        is_on_diet: true,
+      });
+
+    await supertest(app.server)
+      .post('/meals/create')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Almoço',
+        description: 'Comida caseira',
+        date: '2023-01-01',
+        hour: '12:00',
+        is_on_diet: true,
+      });
+
+    await supertest(app.server)
+      .post('/meals/create')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Lanche da tarde',
+        description: 'Coca-cola lata e pastel de frango',
+        date: '2023-01-01',
+        hour: '16:00',
+        is_on_diet: false,
+      });
+
+    const response = await supertest(app.server)
+      .get('/users/me/metrics')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toEqual(200);
+    expect(response.body.metrics).toEqual(
+      expect.objectContaining({
+        registered_meals: 3,
+        best_sequence: 2,
+        current_sequence: 0,
+        off_the_diet: 1,
+        within_the_diet: 2,
+      }),
+    );
   });
 });
